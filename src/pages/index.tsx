4e6d2f19d069166e81dcccb684209a2c5b8934de
Rodrigo Machado Mena <rm.mena86@gmail.com>
2023-06-08 16:30:59 -0300

    #14: Feature - Carregamento de dados do produto com SSG utilizando getStaticPaths

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ interface HomeProps {
     id: string,
     name: string,
     imageUrl: string,
-    price: number
+    price: string
   }[]
 }
 
@@ -55,10 +55,10 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount ? new Intl.NumberFormat('pt-BR', {
+      price: new Intl.NumberFormat('pt-BR', {
         style: 'currency',
         currency: 'BRL'
-      }).format(price.unit_amount / 100) : 0,
+      }).format(price.unit_amount / 100),
     }
   })
 

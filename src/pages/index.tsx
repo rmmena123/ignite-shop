67921ab7773950801b94da2f1d8823741badfe88
Rodrigo Machado Mena <rm.mena86@gmail.com>
2023-06-09 15:52:55 -0300

    #20: Feature - Trabalhando com SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Head from 'next/head'
 import { GetStaticProps } from "next"
 import { stripe } from "../lib/stripe"
 
@@ -27,19 +28,25 @@ export default function Home({ products }: HomeProps) {
   });
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {products.map(product => {
-        return (
-          <Product key={product.id} href={`/product/${product.id}`} prefetch={false} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt={product.name} />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
-        )
-      })}
-    </HomeContainer>
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map(product => {
+          return (
+            <Product key={product.id} href={`/product/${product.id}`} prefetch={false} className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt={product.name} />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          )
+        })}
+      </HomeContainer>
+    </>
   )
 }
 
--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps } from "next";
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head";
 import { ImageContainer, SuccessContainer } from "../../styles/pages/success";
 import Stripe from "stripe";
 import { stripe } from "../../lib/stripe";
@@ -15,21 +16,30 @@ interface SuccessProps {
   
 export default function Success({ costumerName, product }: SuccessProps) {
     return (
-      <SuccessContainer>
-        <h1>Compra efetuada</h1>
-  
-        <ImageContainer>
-          <Image src={product.imageUrl} width={120} height={110} alt="" />
-        </ImageContainer>
-  
-        <p>
-          Uhuul <strong>{costumerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
-        </p>
-  
-        <Link href="/">
-          Voltar ao catálogo
-        </Link>
-      </SuccessContainer>
+      <>
+        <Head>
+          <title>Compra efetuada | Ignite Shop</title>
+
+          <meta name="robots" content="noindex" />
+        </Head>
+
+        <SuccessContainer>
+          <h1>Compra efetuada</h1>
+    
+          <ImageContainer>
+            <Image src={product.imageUrl} width={120} height={110} alt="" />
+          </ImageContainer>
+    
+          <p>
+            Uhuul <strong>{costumerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
+          </p>
+    
+          <Link href="/">
+            Voltar ao catálogo
+          </Link>
+        </SuccessContainer>
+      </>
+      
     )
 }
   
